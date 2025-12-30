5a3bd69701ec1195e180bff7f780ee06dd7df0fc
Loc Nguyen Duc <duclocn@gmail.com>
2025-12-30 15:06:43 +0700

    Add Recent Scores, Opacity transition

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -50,8 +50,26 @@ struct ContentView: View {
                     }//VStack
                     
                     Spacer()
-                    
-                    Spacer()
+                    VStack {
+                        if animateViewIn {
+                            VStack {
+                                Text("Recent Scores")
+                                    .font(.title2)
+                                
+                                Text("33")
+                                Text("27")
+                                Text("15")
+                            }// VStack
+                            .foregroundStyle(.white)
+                            .font(.title3)
+                            .shadow(radius: 5)
+                            .padding(.horizontal)
+                            .background(.black.opacity(0.7))
+                            .clipShape(.rect(cornerRadius: 15))
+                            .transition(.opacity)
+                        }
+                    }
+                    .animation(.linear(duration: 1).delay(4), value: animateViewIn)
                     
                     Spacer()
                     
