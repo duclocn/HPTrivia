9849a5ac7a165c8867e6d06b0eab6d8dffc07b2d
Loc Nguyen Duc <duclocn@gmail.com>
2026-01-01 16:30:21 +0700

    Fix: Use correct question arrays for each book in populateBooks

--- HPTrivia/Classes-Functions/BookQuestionsClass.swift
+++ HPTrivia/Classes-Functions/BookQuestionsClass.swift
@@ -43,11 +43,11 @@ class BookQuestions {
     
     private func populateBooks(with organizedQuestions: [[QuestionModel]]) {
         books.append(BookModel(id: 1, image: "hp1", questions: organizedQuestions[1], status: .active))
-        books.append(BookModel(id: 2, image: "hp2", questions: organizedQuestions[1], status: .active))
-        books.append(BookModel(id: 3, image: "hp3", questions: organizedQuestions[1], status: .inactive))
-        books.append(BookModel(id: 4, image: "hp4", questions: organizedQuestions[1], status: .locked))
-        books.append(BookModel(id: 5, image: "hp5", questions: organizedQuestions[1], status: .locked))
-        books.append(BookModel(id: 6, image: "hp6", questions: organizedQuestions[1], status: .locked))
-        books.append(BookModel(id: 7, image: "hp7", questions: organizedQuestions[1], status: .locked))
+        books.append(BookModel(id: 2, image: "hp2", questions: organizedQuestions[2], status: .active))
+        books.append(BookModel(id: 3, image: "hp3", questions: organizedQuestions[3], status: .inactive))
+        books.append(BookModel(id: 4, image: "hp4", questions: organizedQuestions[4], status: .locked))
+        books.append(BookModel(id: 5, image: "hp5", questions: organizedQuestions[5], status: .locked))
+        books.append(BookModel(id: 6, image: "hp6", questions: organizedQuestions[6], status: .locked))
+        books.append(BookModel(id: 7, image: "hp7", questions: organizedQuestions[7], status: .locked))
     }
 }
